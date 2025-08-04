dbbf63d5b283953826799471bea794d11cf137c8
Farrukh Zeb <43153347+FarrukhXeb@users.noreply.github.com>
2025-08-04 18:21:29 +0500

    List operations

---
+++ list_operations.py
@@ -0,0 +1,9 @@
+numbers = [1, 5, 3, 8, 2]
+
+
+print(f"Sum: {sum(numbers)}")
+print(f"Average: {sum(numbers)/len(numbers)}")
+print(f"Max: {max(numbers)}")
+
+my_list = ['a', 'b', 'c']
+print(f"Reverse: {my_list[::-1]}")
